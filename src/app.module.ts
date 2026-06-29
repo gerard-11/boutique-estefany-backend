d2c2be9805631cb231896b2a8bde8c317b7f5c03
Gerardo J <grrdrito@gmail.com>
2026-06-29 15:04:49 -0600

    refactor: remove wishlit endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,6 @@ import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { PaymentsModule } from './payments/payments.module';
 import { DashboardReportModule } from './dashboard-report/dashboard-report.module';
-import { WishlistModule } from './wishlist/wishlist.module';
 
 @Module({
   imports: [
@@ -29,7 +28,6 @@ import { WishlistModule } from './wishlist/wishlist.module';
     TransactionsModule,
     PaymentsModule,
     DashboardReportModule,
-    WishlistModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/wishlist/dtos/add-to-wishlist.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsUUID, IsNotEmpty } from 'class-validator';
-
-export class AddToWishlistDto {
-  @IsUUID('4')
-  @IsNotEmpty()
-  productId: string;
-}
